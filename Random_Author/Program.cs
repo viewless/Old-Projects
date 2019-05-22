f1e3a417d5dc72461bb89547f670ede81af68d3f
Nikola Dionisiev <36667350+viewless@users.noreply.github.com>
2019-05-22 22:27:30 +0300

    Add files via upload

---
+++ Random_Author/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Random_Author
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> authors = new List<string> { "Алеко Константинов", "Атанас Далчев", "Гео Милев", "Димитър Димов", "Елин Пелин", "Димитър Талев", "Димчо Дебелянов", "Йордан Йовков", "Пейо Яворов", "Елисавета Багряна", "Никола Вапцаров", "Христо Ботев", "Иван Вазов", "Пенчо Славейков", "Христо Смирненски" };
+
+            Random random = new Random();
+            int randomAuthor = random.Next(0, 15);
+
+            string author = authors[randomAuthor];
+
+            Console.WriteLine(author);
+        }
+    }
+}
