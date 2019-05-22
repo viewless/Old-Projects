4f5ac073926fbd07c9d01fbf7c48fc174f64260c
Nikola Dionisiev <36667350+viewless@users.noreply.github.com>
2019-05-22 22:25:29 +0300

    Add files via upload

---
+++ Morse/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Morse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Dictionary<char, string> alphabet = new Dictionary<char, string>
+            {
+                [' '] = "/",
+                ['a']= ".-",
+                ['b']= "-...",
+                ['c']= "-.-.",
+                ['d']= "-..",
+                ['e'] = ".",
+                ['f']= "..-.",
+                ['g']= "--.",
+                ['h']= "....",
+                ['i']= "..",
+                ['j']= ".---",
+                ['k']= "-.-",
+                ['l']= ".-..",
+                ['m'] = "--",
+                ['n']= "-.",
+                ['o']= "---",
+                ['p']= ".--.",
+                ['q']= "--.-",
+                ['r']= ".-.",
+                ['s']= "...",
+                ['t']= "-",
+                ['u']= "..-",
+                ['v']= "...-",
+                ['w']= ".--",
+                ['x']= "-..-",
+                ['y']= "-.--",
+                ['z'] = "--..",
+                ['1']= ".----",
+                ['2']= "..---",
+                ['3']= "...--",
+                ['4'] = "....-",
+                ['5']= ".....",
+                ['6']= "-....",
+                ['7'] = "--...",
+                ['8']= "---..",
+                ['9']= "----.",
+                ['0'] = "-----"
+
+            };
+
+
+           
+
+            string input = Console.ReadLine().ToLower();
+
+            char[] characters = input.ToCharArray();
+
+
+            foreach (char item in characters)
+            {
+                Console.Write(alphabet[item]);
+            }
+            
+
+        }
+    }
+}
